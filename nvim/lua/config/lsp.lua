bed954995fe4b3e0c0e228683516ee981087d575
Ivan-Sanchez-Diaz <ivansanchezdiaz2001@gmail.com>
2022-12-05 13:52:22 -0500

    fix: fix boder in vim.lsp.buf.hover

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -23,3 +23,20 @@ vim.api.nvim_set_option('updatetime', 300)
 vim.api.nvim_set_hl(0, "NormalFloat", {bg="#282828"})
 vim.api.nvim_set_hl(0, "ColorColumn", {bg="#282828"})
 
+local border = {
+        { "╭", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╮", "FloatBorder" },
+        { "│", "FloatBorder" },
+        { "╯", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╰", "FloatBorder" },
+        { "│", "FloatBorder" },
+}
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
