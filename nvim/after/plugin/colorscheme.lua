5071d83fba7767db849ab81fb03bf39f61b64ee4
Ivan-Sanchez-Diaz <ivansanchezdiaz2001@gmail.com>
2023-04-23 00:18:56 -0500

    fix: colorscheme

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,13 +1,13 @@
 require("gruvbox").setup({
     italic = false,
     overrides = {
-        SignColumn = {bg = "#282828"},
-        GruvboxRedSign = { bg = "#282828"},
-        GruvboxGreenSign = { bg = "#282828"},
-        GruvboxYellowSign = { bg = "#282828"},
-        GruvboxBlueSign = { bg = "#282828"},
-        GruvboxPurpleSign = { bg = "#282828"},
-        GruvboxAquaSign = { bg = "#282828"},
+        SignColumn = {bg = "#1d2021"},
+        GruvboxRedSign = { bg = "#cc241d"},
+        GruvboxGreenSign = { bg = "#98971a"},
+        GruvboxYellowSign = { bg = "#d79921"},
+        GruvboxBlueSign = { bg = "#458588"},
+        GruvboxPurpleSign = { bg = "#b16286"},
+        GruvboxAquaSign = { bg = "#689d6a"},
  
     }
 })
@@ -15,3 +15,8 @@ require("gruvbox").setup({
 vim.o.background = "dark" 
 vim.o.termguicolors = true
 vim.cmd([[colorscheme gruvbox]])
+vim.api.nvim_set_hl(0, "Normal", {bg="#1d2021"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg="#1c1c1c"})
+vim.api.nvim_set_hl(0, "FloatBorder", {bg="#1c1c1c"})
+vim.api.nvim_set_hl(0, "ColorColumn", {bg="#1c1c1c"})
+
--- nvim/lua/core/set.lua
+++ nvim/lua/core/set.lua
@@ -2,8 +2,6 @@
 vim.opt.hlsearch = true
 -- ignore case in search patterns
 vim.opt.ignorecase = true
--- use spaces when <Tab> is inserted
-vim.opt.expandtab = true
 -- clear highlight from last search pattern
 vim.keymap.set('n','<leader>h',':nohlsearch<CR>')
 -- print the line number in front of each line
@@ -12,15 +10,17 @@ vim.opt.nu = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.opt.relativenumber = false
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
-vim.opt.shiftwidth = 0
+vim.opt.shiftround = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.clipboard = 'unnamedplus'
 
-vim.api.nvim_set_hl(0, "NormalFloat", {bg="#282828"})
-vim.api.nvim_set_hl(0, "ColorColumn", {bg="#282828"})
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 
 
 local border = {
@@ -74,7 +74,7 @@ vim.wo.signcolumn = "yes"
 
 local format_sync_grp = vim.api.nvim_create_augroup("Format", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = {"*.rs","*.html","*.py","*.js"},
+  pattern = {"*.rs","*.html","*.py","*.js","*.c"},
   callback = function()
     vim.lsp.buf.format({ timeout_ms = 200 })
   end,
