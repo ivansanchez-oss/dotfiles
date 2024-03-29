dde302067cd88a558e93c24221471886eabec1c7
Ivan <ivansanchezdiaz2001@gmail.com>
2024-03-29 14:45:25 -0500

    feat: add .clang-format, .zshrc and fix ident

--- nvim/after/plugin/ident.lua
+++ nvim/after/plugin/ident.lua
@@ -1,13 +1,6 @@
 vim.api.nvim_set_hl(0, "IndentBlanklineIndent", { fg = "#666666", bg = "#1c1c1c" })
 vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { fg = "#C6C6C6", bg = "#1c1c1c" })
 
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    max_indent_increase = 1,
-    show_trailing_blankline_indent = false,
-    use_treesitter = true,
-    char_highlight_list = {
-        "IndentBlanklineIndent",
-    }
+require("ibl").setup {
+    scope = { enabled = false },
 }
